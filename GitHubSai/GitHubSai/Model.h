af25846ab0ed13830b2f1b4c30b09522832c48d1
zhaoyaqin <981371052@qq.com>
2016-01-21 16:05:17 +0800

    zhaoyaqin

---
+++ GitHubSai/GitHubSai/Model.h
@@ -0,0 +1,13 @@
+//
+//  Model.h
+//  GitHubSai
+//
+//  Created by 赵亚琴 on 16/1/21.
+//  Copyright © 2016年 张赛赛. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Model : NSObject
+
+@end
---
+++ GitHubSai/GitHubSai/Model.m
@@ -0,0 +1,13 @@
+//
+//  Model.m
+//  GitHubSai
+//
+//  Created by 赵亚琴 on 16/1/21.
+//  Copyright © 2016年 张赛赛. All rights reserved.
+//
+
+#import "Model.h"
+
+@implementation Model
+
+@end
