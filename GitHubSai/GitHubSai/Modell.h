42b860620291ad95c8d7bfaa5983d19e0bc0e338
张赛赛 <1036811592@qq.com>
2016-01-21 17:11:31 +0800

    adsfasdf

---
+++ GitHubSai/GitHubSai/Modell.h
@@ -0,0 +1,13 @@
+//
+//  Modell.h
+//  GitHubSai
+//
+//  Created by 张赛赛 on 16/1/21.
+//  Copyright © 2016年 张赛赛. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Modell : NSObject
+
+@end
---
+++ GitHubSai/GitHubSai/Modell.m
@@ -0,0 +1,13 @@
+//
+//  Modell.m
+//  GitHubSai
+//
+//  Created by 张赛赛 on 16/1/21.
+//  Copyright © 2016年 张赛赛. All rights reserved.
+//
+
+#import "Modell.h"
+
+@implementation Modell
+
+@end
